0a947bd9bc6d917bfbce0cf97415eea78b00e507
Danyil <danilagarma1999@mail.ru>
2024-06-02 05:56:37 +0300

    finish owl reviews

--- myjs.js
+++ myjs.js
@@ -73,3 +73,42 @@ function playVideo() {
         iframe.src += "&autoplay=1";
     }
 }
+
+
+/////////////////////////////////OWL REVIEWS/////////////////////////////////
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        onTranslated: updateSlider,
+        responsive: {
+            0: {
+                items: 5
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+    function updateSlider(event) {
+        let items = event.item.count;//получае общее количество элементов в карусели.Это включает все видимые и клонированные элементы
+        // ,которые используются для бесконечного циклического прокручивания.
+
+        let item = event.item.index - event.relatedTarget._clones.length / 2;
+        if (item < 0) {
+            item = items - 1;
+        } else if (item >= items) {
+            item = 0;
+        }
+        let slider = $(".slider-reviews");
+        let percentage = (item / items) * 100;
+        slider.css("left", percentage + "%");
+    }
+});
